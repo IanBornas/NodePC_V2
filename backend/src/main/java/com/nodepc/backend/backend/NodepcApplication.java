9ec9e0de97b4267d87999593d3679693045383a9
Farron1227 <fgwffwfe@yahoo.com>
2025-10-08 21:53:02 +0800

    v.10
    change the formatting of the components

---
+++ backend/src/main/java/com/nodepc/backend/backend/NodepcApplication.java
@@ -0,0 +1,13 @@
+package com.nodepc.backend.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodepcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodepcApplication.class, args);
+	}
+
+}
